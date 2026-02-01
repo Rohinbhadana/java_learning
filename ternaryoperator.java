86fd3d10de0f57c3c6515c9dff1259b82a529631
Rohinbhadana <rohinbhadana0@gmail.com>
2026-02-01 15:39:22 +0530

    Add files via upload

---
+++ ternaryoperator.java
@@ -0,0 +1,30 @@
+// using simple if else 
+    // public class ternaryoperator {
+//     public static void main(String[] args) {
+//         int a=7;
+//         int b=10;
+//         int max=0;
+
+//         if(a>b){
+//             max=a;
+//         }else{
+//             max=b;
+//             System.out.println("value of max is"+max);
+//         }
+//     }
+    
+// }
+
+
+//  but using ternary operator we can run this also like this:
+
+// public class ternaryoperator{
+//     public static void main(String[] args) {
+//         int a=9;
+//         int b=5;
+//         int c=0;
+
+//         c=a>b?a:b;
+//         System.out.println("the value of c is"+c);
+//     }
+// }
